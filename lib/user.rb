7b1968a0a2fa8c657f8c826bee935af7aa0ae0e1
dcaba@github <dani.caba@gmail.com>
2016-01-12 20:08:20 +0100

    basic user complete

--- lib/user.rb
+++ lib/user.rb
@@ -1,5 +1,12 @@
 module WeMeet
 	User = Struct.new(:name,:surname,:email)
-	#class user
-	#end
+	class User
+		def initialize(name,surname,email)
+			split_cap_surname = surname.split(" ").map{|word| word.capitalize}
+			super(name.capitalize, split_cap_surname.join(" "), email)
+		end
+		def to_s
+			"#{name} #{surname}"
+		end
+	end
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -5,8 +5,13 @@ module WeMeet
 		before do
 			@user = User.new("daniel","caballero rodriguez","dani.caba@gmail.com")
 		end
-		it "contains its name and surnames capitalized"
-		it "prints his complete name as expected"
+		it "contains its name and surnames capitalized" do
+			expect(@user.name).to be == "Daniel"
+			expect(@user.surname).to be == "Caballero Rodriguez"
+		end
+		it "prints his complete name as expected" do
+			expect(@user.to_s).to be == "Daniel Caballero Rodriguez"
+		end
 
 	end
 end
