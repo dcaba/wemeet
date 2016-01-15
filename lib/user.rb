6d1d39646222ee37c86f50f34a4cd0ab06c77512
dcaba@github <dani.caba@gmail.com>
2016-01-15 13:27:56 +0100

    use case not working... really weird

--- lib/user.rb
+++ lib/user.rb
@@ -1,9 +1,19 @@
 module WeMeet
-	User = Struct.new(:name,:surname,:email)
+	User = Struct.new(:name,:surname,:email,:password)
+	def self.capitalize_sentence(sentence)
+			sentence.split(" ").map{|word| word.capitalize}.join(" ")
+	end
+
 	class User
-		def initialize(name,surname,email)
-			split_cap_surname = surname.split(" ").map{|word| word.capitalize}
-			super(name.capitalize, split_cap_surname.join(" "), email)
+		def initialize(name,surname,email,password)
+			super(name.capitalize,WeMeet::capitalize_sentence(surname),email,password)
+		end
+		def login(suplied_pass)
+			puts "sup: #{suplied_pass}, ive:#{@password}"
+			@password == suplied_pass
+		end
+		def change_pass(new_pass)
+			@password = new_pass
 		end
 		def to_s
 			"#{name} #{surname}"
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -3,7 +3,8 @@ require 'user'
 module WeMeet 
 	describe User do
 		before do
-			@user = User.new("daniel","caballero rodriguez","dani.caba@gmail.com")
+			@pass = "mypassword"
+			@user = User.new("daniel","caballero rodriguez","dani.caba@gmail.com",@pass)
 		end
 		it "contains its name and surnames capitalized" do
 			expect(@user.name).to be == "Daniel"
@@ -11,6 +12,16 @@ module WeMeet
 		end
 		it "prints his complete name as expected" do
 			expect(@user.to_s).to be == "Daniel Caballero Rodriguez"
+		end
+		it "can authenticate" do
+			puts @user.inspect
+			expect(@user.login(@pass)).to be true
+		end
+		it "can change the password" do
+			new_pass="mynewpassword"
+			@user.change_pass(new_pass)
+			expect(@user.login(@pass)).to be false
+			expect(@user.login(new_pass)).to be true
 		end
 
 	end
