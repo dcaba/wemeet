c7d7074492e53ff0c1ee4ca6f9c9302eb3bf094e
dcaba@github <dani.caba@gmail.com>
2016-03-26 11:53:43 +0100

    activities classes progressing to unlock additional user and group behaviour

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,12 +1,22 @@
 module WeMeet
 
 	class Activity
-		attr_accessor :name
+		attr_accessor :name, :aliases
 		def initialize(name)
 			@name = name
+			@aliases = [name.downcase]
 		end
+		
+		def alias(aname)
+			@aliases << aname.downcase unless @aliases.include? aname.downcase
+		end
+
 		def similar_to?(other)
-			return @name.downcase.include? other.downcase
+			found = false
+			@aliases.each do |as|
+				found = true if as.include? other.downcase
+			end
+			return found
 		end
 	end
 end
--- spec/activity_spec.rb
+++ spec/activity_spec.rb
@@ -13,6 +13,22 @@ module WeMeet
 				expect(@activity).to be_similar_to "footbal"
 			end
 		end
-		context "with aliases"
+		context "with aliases" do
+			before do
+				@activity = Activity.new("Network gaming")
+				@activity.alias("Netgaming")
+				@activity.alias("LAN parties")
+			end
+			it "has the expected aliases" do
+				expect(@activity.aliases).to include "Netgaming"
+				expect(@activity.aliases).to include "LAN parties"
+			end
+			it "support partial text searches" do
+				expect(@activity).to be_similar_to "gaming"
+				expect(@activity).to be_similar_to "LAN"
+				expect(@activity).to be_similar_to "network"
+				expect(@activity).to be_similar_to "netgaming"
+			end
+		end
 	end
 end
