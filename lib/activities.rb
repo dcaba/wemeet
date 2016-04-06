126d503afa0fca0737aa0388b3bc0586d2873a43
dcaba@github <dani.caba@gmail.com>
2016-04-06 09:15:29 +0200

    activities behaviour pretty complete, but we need to categorize activities

--- lib/activities.rb
+++ lib/activities.rb
@@ -8,5 +8,20 @@ module WeMeet
 		def search(term)
 			return self.select {|act| act.similar_to? term}
 		end
+
+		def remove(term)
+			self.reject! { |act| act.name == term }
+			return
+		end
+
+		def clean
+			self.reject! { true}
+		end
+
+		def list
+			activities_name = []
+			self.each { |act| activities_name << act.name }
+			return activities_name
+		end
 	end
 end
--- spec/activities_spec.rb
+++ spec/activities_spec.rb
@@ -16,6 +16,16 @@ module WeMeet
 				expect(@act.search "").to be == []
 			end
 		end
+		context "with an activity, but after cleaning" do
+			before do
+				@act = Activities.instance
+				@act << Activity.new("Football")
+			end
+			it "do not contain any activity after clean" do
+				@act.clean
+				expect(@act.size).to be == 0
+			end
+		end
 		context "with a couple of activities" do
 			before do
 				@activity1 = Activity.new("Football")
@@ -24,12 +34,37 @@ module WeMeet
 				@act << @activity1
 				@act << @activity2
 			end
-			it "support the removal of one activity"
-			it "supports the addition of a new activity"
-			it "has the expected activites" 
+			after do
+				@act.clean
+			end
+			it "support the removal of one activity" do
+				@act.remove("Basketball")
+				expect(@act.list).to include "Football"
+				expect(@act.list).not_to include "Basketball"
+				expect(@act.list.size).to be == 1
+			end
+			it "supports the addition of a new activity" do
+				@activity3 = Activity.new("Playing instrument")
+				@act << @activity3
+				expect(@act.list).to include "Football"
+				expect(@act.list).to include "Basketball"
+				expect(@act.list).to include "Playing instrument"
+				expect(@act.list.size).to be == 3
+			end
 			it "support partial text searches" do
 				expect(@act.search("ball").size).to be == 2
 			end
+		end
+		context "with 4 activities in 2 categories" do
+			before do
+				@activity1 = Activity.new("Football")
+				@activity2 = Activity.new("Basketball")
+				@act = Activities.instance
+				@act << @activity1
+			end
+			it "can categorize activities"
+			it "can retrieve activities per category"
+			it "support partial text searches with category filters"
 		end
 	end
 end
