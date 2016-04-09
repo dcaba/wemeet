c195f9accd6ecefb3b387bee810e08e07574becd
dcaba@github <dani.caba@gmail.com>
2016-04-09 22:17:58 +0200

    swapping ruby versions. Coverage addition to categories

--- spec/activity_category_spec.rb
+++ spec/activity_category_spec.rb
@@ -8,9 +8,11 @@ module WeMeet
 			end
 			it "has the expected category name" do
 				expect(@category.name).to be == "Sports"
+				expect(@category.name).not_to be == "sports"
 			end
 			it "support partial text searches" do
-				expect(@category).to be_similar_to "sport"
+				expect(@category).to be_similar_to "sPort"
+				expect(@category).not_to be_similar_to "Music"
 			end
 		end
 	end
