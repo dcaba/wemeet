80a7699d391bd87197d8ff7b8a79291c0cac1f94
dcaba@github <dani.caba@gmail.com>
2016-10-04 23:35:36 +0200

    basic api client skeleton

--- lib/api_client.py
+++ lib/api_client.py
@@ -1,12 +1,15 @@
 import http.client
 
+host = "localhost"
+port = 8888
+timeout = 5
 
 # main()
-conn = http.client.HTTPConnection('localhost', 8888, timeout=10)
+conn = http.client.HTTPConnection(host, port, timeout=timeout)
 conn.request('GET', '/')
 response = conn.getresponse()
 
 data = response.read()
 
-print(response.status, response.reason)
-print(data)
+print("HTTP response to {0}:{1} : {2} - {3} ".format(host, 
+	port, response.status, response.reason))
